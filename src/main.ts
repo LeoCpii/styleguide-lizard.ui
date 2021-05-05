b95fe91b4a7ecd45301f2222abd1361670e0fbcb
leonardo.goncalves <leogoncalves.contato@gmail.com>
2021-05-05 00:32:40 -0300

    🔨Adiciona versão no meta

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { version } from './../package.json';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,6 +8,15 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+
+function setVersion() {
+  var meta = document.createElement('meta');
+  meta.name = 'version';
+  meta.content = version;
+  document.getElementsByTagName('head')[0].appendChild(meta);
+}
+
+setVersion();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
