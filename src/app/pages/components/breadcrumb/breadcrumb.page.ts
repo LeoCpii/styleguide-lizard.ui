68cf5638418ce16607e7e78052431a2af323d963
leonardo.goncalves <leogoncalves.contato@gmail.com>
2022-01-06 00:37:39 -0300

    🐛 remove import de aquivo inexistente

--- src/app/pages/components/breadcrumb/breadcrumb.page.ts
+++ src/app/pages/components/breadcrumb/breadcrumb.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { SLIDE_Y_SIMPLE } from 'src/app/shared/animations/slide.animation';
-import { BREADCRUMB } from './breadcrumb.const';
 
 @Component({
 	selector: 'app-breadcrumb-page',
